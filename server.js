503dc39a3a36d023240ab87fce0a6a3a5d1fe6f1
M404ttis <mattisluecke@gmail.com>
2023-02-27 23:45:06 +0100

    deletes comments and implemets relative routing

--- server.js
+++ server.js
@@ -1,55 +1,47 @@
-require('dotenv').config();
-// const apiKey = process.env.OPEN_WHEATHER_API_KEY;
-// console.log(apiKey);
-
-
 /* Empty JS object to act as endpoint for all routes */
 projectData = [];
 
 /* Express to run server and routes */
-const express = require('express');
+const express = require("express");
 
 /* Start up an instance of app */
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 /* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 /* Initialize the main project folder*/
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 const port = 8080;
 /* Spin up the server*/
 const server = app.listen(port, listening);
- function listening(){
-    // console.log(server);
-    console.log(`running on localhost:${port}`);
-  };
+function listening() {
+  console.log(`running on localhost:${port}`);
+}
 
 // GET route
-app.get('/allData', sendData);
+app.get("/allData", sendData);
 
-function sendData (request, response) {
+function sendData(request, response) {
   console.log(projectData);
   response.send(projectData);
-};
+}
 
 // POST route
-app.post('/add', addPostCallback);
+app.post("/add", addPostCallback);
 
-function addPostCallback(req,res){
-  res.send('POST received');
+function addPostCallback(req, res) {
   let newData = req.body;
   let newEntry = {
     date: newData.date,
     temp: newData.temp,
-    content: newData.content
-  }
+    content: newData.content,
+  };
   projectData.push(newEntry);
-  // res.send(newData);
-};
+}
--- website/app.js
+++ website/app.js
@@ -1,123 +1,115 @@
-// require('dotenv').config();
-
-// const apiKey = `${process.env.OPEN_WHEATHER_API_KEY}&units=imperial`;
-// const apiKey = process.env.OPEN_WHEATHER_API_KEY;
-// console.log(apiKey);
-const apiKey = '75f585b70ff23db9d3c8d4f79efbd4ac'
-let city = '';
+const apiKey = "";
+let city = "";
 let limit = 1;
 
+function performAction(e) {
+  let currentCity = document.getElementById("cityName").value;
 
-function performAction(e){
-  let currentCity = document.getElementById('cityName').value;
-
-  const getGeoEncodingWeatherAndUpdateUI =  async () =>{
-    if (currentCity === ''){
-      document.getElementById('temp').innerHTML = `We need a city name to get the weather`
-      document.getElementById('date').innerHTML = `Date is: ${getCurrentDate()}`;
-      postData('http://localhost:8080/add', {date: getCurrentDate(), temp: '', content: document.getElementById('entry').value});
-    } else{
+  const getGeoEncodingWeatherAndUpdateUI = async () => {
+    if (currentCity === "") {
+      document.getElementById(
+        "temp"
+      ).innerHTML = `We need a city name to get the weather`;
+      document.getElementById(
+        "date"
+      ).innerHTML = `Date is: ${getCurrentDate()}`;
+      postData("add", {
+        date: getCurrentDate(),
+        temp: "",
+        content: document.getElementById("entry").value,
+      });
+    } else {
       const latAndLonArray = await getGeoEncoding(currentCity);
       const dataWeather = await getWeatherDataWithGeoData(latAndLonArray);
-      postData('http://localhost:8080/add', {date: getCurrentDate(), temp: dataWeather, content: document.getElementById('entry').value});
+      postData("add", {
+        date: getCurrentDate(),
+        temp: dataWeather,
+        content: currentCity,
+      });
       updateUI(dataWeather);
     }
   };
 
   getGeoEncodingWeatherAndUpdateUI();
-};
-
-
-const getGeoEncoding = async (currentCity) => {
-  // let currentCity = document.getElementById('cityName').value;
-  geoEncodingURL = `https://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=${limit}&appid=${apiKey}`;
-  // console.log(geoEncodingURL);
-  const res = await fetch(geoEncodingURL)
-  try {
-    const dataGeoEncoding = await res.json();
-    const {lat, lon} = dataGeoEncoding[0];
-    // console.log(lat, lon);
-    return latAndLon = [lat, lon];
-
-  } catch(error) {
-    console.log("error", error);
-
-  }
 }
 
+const getGeoEncoding = async (currentCity) => {
+  geoEncodingURL = `https://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=${limit}&appid=${apiKey}`;
+  const res = await fetch(geoEncodingURL);
+  try {
+    const dataGeoEncoding = await res.json();
+    const { lat, lon } = dataGeoEncoding[0];
+    return (latAndLon = [lat, lon]);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-const getWeatherDataWithGeoData = async (latAndLon)=> {
-
+const getWeatherDataWithGeoData = async (latAndLon) => {
   const [lat, lon] = latAndLon;
   let weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  // console.log(weatherURL);
-  
-    const res = await fetch(weatherURL)
-    try {
-      const dataWeather = await res.json();
-      // console.log(dataWeather);
-      temp = dataWeather.main.temp
-      // return dataWeather;
-      return temp;
-    } catch (error) {
-      console.log('error', error);
-    }
-  };
-  
+
+  const res = await fetch(weatherURL);
+  try {
+    const dataWeather = await res.json();
+    temp = dataWeather.main.temp;
+    return temp;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 // Example POST method implementation:
 async function postData(url = "", data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-// TODO-Call Function
-// postData('/add', {animal:'lion'})
+const updateUI = (temp) => {
+  let currentCity = document.getElementById("cityName").value;
 
-
-
-const updateUI =  (temp) => {
-    let currentCity = document.getElementById('cityName').value;
-
-  try{
-    document.getElementById('date').innerHTML = `Date is: ${getCurrentDate()}`;
-    document.getElementById('temp').innerHTML = `It is ${temp} Fahrenheit in ${currentCity}`;
-    
-  }catch(error){
+  try {
+    document.getElementById("date").innerHTML = `Date is: ${getCurrentDate()}`;
+    document.getElementById(
+      "temp"
+    ).innerHTML = `It is ${temp} Fahrenheit in ${currentCity}`;
+  } catch (error) {
     console.log("error", error);
   }
-}
+};
 
 // Helper functions
 const getCurrentDate = () => {
   let d = new Date();
-  let newDate = d.getDate()+'/'+ d.getMonth()+'/'+ d.getFullYear();
+  let newDate = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear();
   return newDate;
-}
+};
 
 const updateUserInput = () => {
-  let entry = document.getElementById('entry').value;
-  if (entry === '') {
-    document.getElementById('userResponse').innerHTML = `You didn't enter anything`;
+  let entry = document.getElementById("entry").value;
+  if (entry === "") {
+    document.getElementById(
+      "userResponse"
+    ).innerHTML = `You didn't enter anything`;
   } else {
-    document.getElementById('userResponse').innerHTML = `It seems you feel ${entry}`;
+    document.getElementById(
+      "userResponse"
+    ).innerHTML = `It seems you feel ${entry}`;
   }
-}
+};
 
-
-  // Add event listeners to buttons
-  document.getElementById('fetchWeatherButton').addEventListener('click', performAction);
-  document.getElementById('postEntryButton').addEventListener('click', performAction);
+// Add event listeners to buttons
+document
+  .getElementById("fetchWeatherButton")
+  .addEventListener("click", performAction);
+document
+  .getElementById("postEntryButton")
+  .addEventListener("click", performAction);
